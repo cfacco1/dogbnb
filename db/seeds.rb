b8e1aa1a92bb279239a637f868db84703c7f7f46
cfacco1 <facco.clement@gmail.com>
2020-04-30 15:55:25 +0200

    Finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Dog.destroy_all
+Dogsitter.destroy_all
+City.destroy_all
+Stroll.destroy_all
+
+50.times do
+  Dogsitter.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+50.times do
+  Dog.create!(
+    dog_name: Faker::Creature::Dog.name
+  )
+end
+
+15.times do
+  City.create!(
+    city_name: Faker::Address.city
+  )
+end
+
+100.times do
+  Stroll.create!(
+    dog_id: Dog.all.sample.id,
+    dogsitter_id: Dogsitter.all.sample.id,
+    city_id: City.all.sample.id
+  )
+end
